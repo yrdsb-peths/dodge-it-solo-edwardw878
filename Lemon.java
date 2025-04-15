b3a8293a0cf59ddc814ebae4b3aa398ba83199ec
edwardw878 <edwardwang878@gmail.com>
2025-04-15 14:27:13 -0400

    lemon move + reset

--- Lemon.java
+++ Lemon.java
@@ -9,6 +9,23 @@ public class Lemon extends Actor
      */
     public void act()
     {
-        move(-3);
+        move(-10);
+        
+        if (getX() <= 0)
+        {
+            resetLemon();
+        }
+    }
+    
+    public void resetLemon()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if (num == 0)
+        {
+            setLocation(600, 100);    
+        } else {
+            setLocation(600, 300);    
+        }
+        
     }
 }
