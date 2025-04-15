b870c23a3f495c5f883da9c2abf199660ac00bed
edwardw878 <edwardwang878@gmail.com>
2025-04-15 14:22:55 -0400

    added lemon

---
+++ Lemon.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Lemon extends Actor
+{
+    /**
+     * Act - do whatever the Lemon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(-3);
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -6,5 +6,8 @@ public class MyWorld extends World {
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
+        
+        Lemon lemon = new Lemon();
+        addObject (lemon, 600, 100);
     }
 }
